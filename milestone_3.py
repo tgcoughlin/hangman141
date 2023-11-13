838bbc252af191329467d09bd5b03e2817271865
Tom <tgcoughlin@gmail.com>
2023-11-13 17:13:56 +0000

    final updates to milestone3

--- milestone_3.py
+++ milestone_3.py
@@ -1,9 +1,30 @@
-while True:
-            guess = input("Please guess a letter: ").lower()
-            
-            if guess.isalpha():
-                   break
+word = "apple"
+
+
+def check_guess(guess):
+    
+    if guess.lower() in word:
+            print(f"Good guess, the letter {guess} is in the word!")
+    else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+
+def ask_for_input():
+    while True:
+                guess = input("Please guess a letter: ")
                 
-                           
-            else:
-                print("Invalid letter. Please, enter a single alphabetical character.")
+                if guess.isalpha():
+                    check_guess(guess)
+                    return guess
+                    
+                            
+                else:
+                    print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    
+
+ask_for_input()
+
+
+
