7c75367965beb6f97289bc69f24ef7be5de334b6
Tom <tgcoughlin@gmail.com>
2023-11-16 08:54:24 +0000

    Updates to milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -19,6 +19,7 @@ class Hangman:
                     self.word_guessed[i] = guess
 
             self.num_letters -= 1 
+            print(self.word_guessed) 
             
         else:
             print(f"Sorry {guess} is not in the word")
